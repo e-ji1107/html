e742b6f3a908a74e2f7862064bf3897e8c387220
e-ji1107 <diddmswl1107@gmail.com>
2025-05-21 16:58:02 +0900

    js_0521

---
+++ ex18-01a.js
@@ -0,0 +1,19 @@
+const arr = [11,22,33,44,55]
+const arr2 = arr
+console.log(arr)
+arr2[2]=7
+console.log(arr, arr2)
+console.log(arr==arr2)
+console.log(arr===arr2)
+
+// 깊은복사
+const srr =  [66,77,88,99,0]
+const arr3 = [...srr]
+console.log(srr,arr3)
+console.log(srr==arr3)
+console.log(srr===arr3)
+
+console.log(Math.max(1,2,3,4,11,22,33,44,77,34))
+console.log(Math.max(arr)) // NaN
+console.log(Math.max(...arr)) 
+console.log(Math.min(...arr)) 
---
+++ ex18-01b.js
@@ -0,0 +1,4 @@
+function fn (...arr){ // 나머지 매개변수수
+    console.log(...arr) // 전개 연산자
+}
+fn(1,2,3,4,5,6)
